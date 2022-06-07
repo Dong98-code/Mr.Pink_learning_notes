22b32af196cde79f10ba6c15616e3e0e58826c02
”Dong98-code“ <1094108253@qq.com>
2022-06-07 22:11:35 +0800

    watch函数

---
+++ vue/学习代码/vue3_test/08_src_watch函数/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <MyDemo></MyDemo>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+import MyDemo from './components/Demo.vue'
+export default {
+  name: 'App',
+  // 暂时不考虑 响应式
+  components:{MyDemo}
+}
+</script>
+
