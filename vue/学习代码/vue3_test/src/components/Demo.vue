c259ddfcce542c0b47f4787aabf59fad141a24a1
”Dong98-code“ <1094108253@qq.com>
2022-06-07 21:04:04 +0800

    setup的注意点

---
+++ vue/学习代码/vue3_test/src/components/Demo.vue
@@ -0,0 +1,52 @@
+<template>
+  
+  <h2>年龄：{{person.age }}</h2>
+  <h2>姓名:{{person.name}}</h2>
+  <slot name="qwe"></slot>
+  <button @click="sayHello">点击触发hello</button>
+
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import {reactive} from 'vue'
+export default {
+  name: 'MyDemo',
+  // 暂时不考虑 响应式
+  // beforeCreate() {
+  //   console.log('---beforeC');
+  // },
+  props:['msg', 'school'],
+  emits:['hello'], // 自定义事件传递
+  setup(props, context) {
+    console.log('----setup---', this);
+
+    console.log(context.slots);
+
+    let person = reactive({
+      name: '张三',
+      age: 10,
+      job: {
+      type:'前端',
+      salary:'20k',
+      a:{
+        c:10
+      }
+    },
+    hobby:['吸烟', '喝酒']
+
+    })
+
+    // sayH
+    function sayHello() {
+      context.emit('hello', 666)
+    }
+    // 返回对象
+    return {
+      person,
+      sayHello
+    }
+  }
+}
+</script>
+
--- vue/学习代码/vue3_test/vue3快速上手.md
+++ vue/学习代码/vue3_test/vue3快速上手.md
@@ -361,12 +361,24 @@ proxy.name = 'tom'
 
 - setup执行的时机
   - 在beforeCreate之前执行一次，this是undefined。
-  
+    ![20220607203655](https://xd-imgsubmit.oss-cn-beijing.aliyuncs.com/images/20220607203655.png)
+  - 声明周期钩子
+    ![20220607202744](https://xd-imgsubmit.oss-cn-beijing.aliyuncs.com/images/20220607202744.png)
+
+
+传参之后 不使用会警告：
+
+![20220607204003](https://xd-imgsubmit.oss-cn-beijing.aliyuncs.com/images/20220607204003.png)
 - setup的参数
   - props：值为对象，包含：组件外部传递过来，且组件内部声明接收了的属性。
+  > 1. 响应式对象 , 不能使用es6解构
+
+  
   - context：上下文对象
     - attrs: 值为对象，包含：组件外部传递过来，但没有在props配置中声明的属性, 相当于 ```this.$attrs```。
     - slots: 收到的插槽内容, 相当于 ```this.$slots```。
+
+    
     - emit: 分发自定义事件的函数, 相当于 ```this.$emit```。
 
 
