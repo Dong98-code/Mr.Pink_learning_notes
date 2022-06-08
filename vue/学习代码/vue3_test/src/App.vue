db16d9d830c9abb682de0623d47ad12c8488397f
”Dong98-code“ <1094108253@qq.com>
2022-06-08 11:21:12 +0800

    生命周期

--- vue/学习代码/vue3_test/src/App.vue
+++ vue/学习代码/vue3_test/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <MyDemo></MyDemo>
+<button @click="isShowDemo = !isShowDemo">点击显示/隐藏</button>
+  <MyDemo v-if="isShowDemo"></MyDemo>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 import MyDemo from './components/Demo.vue'
+import {ref} from 'vue'
 export default {
   name: 'App',
   // 暂时不考虑 响应式
-  components:{MyDemo}
+  components:{MyDemo},
+  setup() {
+    
+    const isShowDemo = ref(true)
+    
+    return {
+      isShowDemo
+    }
+  }
 }
 </script>
 
--- vue/学习代码/vue3_test/src/components/Demo.vue
+++ vue/学习代码/vue3_test/src/components/Demo.vue
@@ -4,92 +4,78 @@
   <br />
   <h2>当前信息时：{{ msg }}</h2>
   <button @click="msg += '!'">点击+'!'</button>
-  <br>
-  <h2>name:{{person.name}}</h2>
-  <h3>age:{{person.age}}</h3>
-  <button @click="person.name+='--'">修改姓名</button>
-  <button @click="person.age++">年龄增长</button>
+  <br />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import { ref, watch , reactive, watchEffewatchEffect, watchEffect} from "vue";
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUnmount,
+  onUnmounted,
+  onBeforeUpdate,
+  onUpdated,
+} from "vue";
 export default {
   name: "MyDemo",
 
-  // watch: {
-  //   // sum(newValue, oldValue) {
-  //   //   console.log('sum变化', newValue, oldValue);
-  //   // }
-  //   sum: {
-  //     immediate:true,
-  //     deep:true,// 深度监视
-  //     handler(newValue, oldValue) {
-  //       console.log("sum变化", newValue, oldValue);
-  //     },
-  //   },
-  // },
-  setup(props, context) {
+  setup() {
+    console.log('---set up');
     let sum = ref(0);
     let msg = ref("你好");
-    const person = reactive({
-      name:'xdd',
-      age:10
-    })
-    // // 监视， 函数
-    // // 监视ref定义的响应式数据
-    // watch(sum, (newValue, oldValue) => {
-    //   console.log('sum变化',newValue, oldValue);
-    // },{immediate:true})
 
-    // 2. 监视 ref定义的多个数据, 调用多次watch函数
-    // watch(msg, (newValue, oldValue) => {
-    //   console.log("msg变化", newValue, oldValue);
-    // });
-
-    // 数组
-    // watch([msg, sum], (newValue, oldValue) => {
-    //   console.log("数据变化", newValue, oldValue);
-    // });
-
-    // 3. reactive 监视对象
-    // 返回对象
-    // watch(
-    //   () => person, 
-    //   (person, newPerson) => {
-    //   console.log('person变化', person, newPerson);
-      
-    // },{deep:true})
-
-    // // 4. 监视reactive某一个属性
-    // watch(
-    //   () => person.name, 
-    //   (name, newName) => {
-    //   console.log('name变化了', name, newName);
-      
-    // },{deep:true})
-    // // 5. 监视reactive 某一些数据
-    // watch(
-    //   [() => person.name, () => person.age], 
-    //   (name, newName) => {
-    //   console.log('name变化了', name, newName);
-      
-    // },{deep:true})
-
-    // watchEffect, 不用 声明监视对象
-    watchEffect(() => {
-      console.log('watcheffect执行了');
-      // 在回调函数里面 用到了什么 监视什么
-      const x1 = sum.value;
-      const x2 = person.name;
-    })
-
+    onBeforeMount(() => {
+      console.log("----onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("----onMounted");
+    });
+    onBeforeUnmount(() => {
+      console.log("----onBeforeUnmount");
+    });
+    onUnmounted(() => {
+      console.log("----onUnmounted");
+    });
+    onBeforeUpdate(() => {
+      console.log("----onBeforeUpdate");
+    });
+    onUpdated(() => {
+      console.log("----onUpdated");
+    });
     return {
-      person,
       msg,
-      sum
+      sum,
     };
   },
+  //#region
+
+  // beforeCreate() {
+  //   console.log('---beforeCreate---');
+  // },
+  // created() {
+  //   console.log('---Create---');
+  // },
+  // beforeMount() {
+  //   console.log('---beforeMount---');
+  // },
+  // mounted() {
+  //   console.log('---Mounted---');
+  // },
+  // beforeUpdate() {
+  //   console.log('---beforeUpdated---');
+  // },
+  // updated() {
+  //   console.log('---updataed---');
+  // },
+  // beforeUnmount() {
+  //   console.log('---beforeUnMount---');
+  // },
+  // unmounted() {
+  //   console.log('---unMounted---');
+  // }
+  //#endregion
 };
 </script>
 
