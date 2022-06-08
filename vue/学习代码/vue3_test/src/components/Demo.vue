93be9d4ad4c6754bbc550f0561051818db2bff99
”Dong98-code“ <1094108253@qq.com>
2022-06-08 10:52:10 +0800

    watchEffect函数

--- vue/学习代码/vue3_test/src/components/Demo.vue
+++ vue/学习代码/vue3_test/src/components/Demo.vue
@@ -13,7 +13,7 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import { ref, watch , reactive} from "vue";
+import { ref, watch , reactive, watchEffewatchEffect, watchEffect} from "vue";
 export default {
   name: "MyDemo",
 
@@ -38,9 +38,9 @@ export default {
     })
     // // 监视， 函数
     // // 监视ref定义的响应式数据
-    watch(sum, (newValue, oldValue) => {
-      console.log('sum变化',newValue, oldValue);
-    },{immediate:true})
+    // watch(sum, (newValue, oldValue) => {
+    //   console.log('sum变化',newValue, oldValue);
+    // },{immediate:true})
 
     // 2. 监视 ref定义的多个数据, 调用多次watch函数
     // watch(msg, (newValue, oldValue) => {
@@ -54,27 +54,36 @@ export default {
 
     // 3. reactive 监视对象
     // 返回对象
-    watch(
-      () => person, 
-      (person, newPerson) => {
-      console.log('person变化', person, newPerson);
+    // watch(
+    //   () => person, 
+    //   (person, newPerson) => {
+    //   console.log('person变化', person, newPerson);
       
-    },{deep:true})
+    // },{deep:true})
 
-    // 4. 监视reactive某一个属性
-    watch(
-      () => person.name, 
-      (name, newName) => {
-      console.log('name变化了', name, newName);
+    // // 4. 监视reactive某一个属性
+    // watch(
+    //   () => person.name, 
+    //   (name, newName) => {
+    //   console.log('name变化了', name, newName);
       
-    },{deep:true})
-    // 5. 监视reactive 某一些数据
-    watch(
-      [() => person.name, () => person.age], 
-      (name, newName) => {
-      console.log('name变化了', name, newName);
+    // },{deep:true})
+    // // 5. 监视reactive 某一些数据
+    // watch(
+    //   [() => person.name, () => person.age], 
+    //   (name, newName) => {
+    //   console.log('name变化了', name, newName);
       
-    },{deep:true})
+    // },{deep:true})
+
+    // watchEffect, 不用 声明监视对象
+    watchEffect(() => {
+      console.log('watcheffect执行了');
+      // 在回调函数里面 用到了什么 监视什么
+      const x1 = sum.value;
+      const x2 = person.name;
+    })
+
     return {
       person,
       msg,
