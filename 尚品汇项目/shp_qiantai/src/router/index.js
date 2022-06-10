6b6e4b2bad69f80c4941df543619e97eef7ebf23
”Dong98-code“ <1094108253@qq.com>
2022-06-10 20:06:18 +0800

    重写 push 和replace 函数

--- 尚品汇项目/shp_qiantai/src/router/index.js
+++ 尚品汇项目/shp_qiantai/src/router/index.js
@@ -2,10 +2,10 @@
 
 // 引入vue , router
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 
 // 使用插件
-Vue.use(Router)
+Vue.use(VueRouter)
 
 // 
 import MyHome from '@/pages/Home'
@@ -14,7 +14,33 @@ import MySearch from '@/pages/Search'
 import MyRegister from '@/pages/Register'
 // 暴露 路由
 
-const router = new Router({
+
+// 重写push
+// 保存原始
+let originPush = VueRouter.prototype.push;
+let originReplace = VueRouter.prototype.replace;
+// 重写push
+VueRouter.prototype.push = function (location, resolve, reject) {
+    // location 为原本传入的目标 
+    if (reject && resolve) {
+        // call 和 apply传参的方式不一样， call用逗号隔开
+        originPush.call(this, location, resolve, reject)
+    } else {
+        originPush.call(this, location, ()=>{}, ()=>{}) // 手动传入两个回调函数 占位
+    }
+}
+
+VueRouter.prototype.replace = function (location, resolve, reject) {
+    // location 为原本传入的目标 
+    if (reject && resolve) {
+        // call 和 apply传参的方式不一样， call用逗号隔开
+        originReplace.call(this, location, resolve, reject)
+    } else {
+        originReplace.call(this, location, ()=>{}, ()=>{}) // 手动传入两个回调函数 占位
+    }
+}
+
+const router = new VueRouter({
     // 配置路由
     routes: [
         {   
