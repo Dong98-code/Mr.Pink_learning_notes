e58f4b42f547f4b0b3abec23e12915f7145718a1
”Dong98-code“ <1094108253@qq.com>
2022-07-15 17:21:05 +0800

    品牌管理 包括删除 修改 表单验证等

--- 推电影_/vue-cli3 demo/src/views/movieList/index.vue
+++ 推电影_/vue-cli3 demo/src/views/movieList/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <HeadTop head-title="电影列表" go-back="true"></HeadTop>
+    <Test/>
   </div>
 </template>
 
 <script>
 import HeadTop from "@/components/header/Head";
+import Test from "./test"
 import {mapGetters} from 'vuex'
 export default {
     name:"movieList",
-    components:{HeadTop},
+    components:{HeadTop, Test},
     mounted() {
         this.$store.dispatch("get_movie_list_info");
         
---
+++ 推电影_/vue-cli3 demo/src/views/movieList/test/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class='dialog' @click='hideMask' ref='content'>
+        <slot></slot>
+    </div>
+</template>    
+ 
+<script>
+export default {
+  data(){
+    return{
+ 
+    }
+  },
+  methods:{
+    hideMask(event){
+      let event_1 = event.currentTarget
+      let event_2 = event.target
+      if( event_1== event_2){
+        this.$emit("hideMask")
+      }
+    },
+  }
+}
+</script>
+ 
+<style lang="scss" scope>
+    .dialog{
+      position: fixed;
+      z-index: 30;
+      height: 100vh;
+      width: 100vh;
+      display: flex;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-50%);
+      background-color: rgba(44, 52, 60,.7);
+       
+    }
+    
+</style>
