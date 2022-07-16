40d8958b4800c9a611e9d6fe7ab08d771fe8536e
”Dong98-code“ <1094108253@qq.com>
2022-07-16 17:40:59 +0800

    平台属性-修改属性和添加属性；编辑查看切换

--- 尚品汇项目/shp_houtai/vue-admin-template/src/views/product/Attr/index.vue
+++ 尚品汇项目/shp_houtai/vue-admin-template/src/views/product/Attr/index.vue
@@ -44,11 +44,12 @@
             width="150"
             label="操作"
           >
-            <template slot-scope="{}">
+            <template slot-scope="{ row }">
               <el-button
                 type="warning"
                 icon="el-icon-edit"
                 size="mini"
+                @click="updateAttr(row)"
               ></el-button>
               <el-button
                 type="danger"
@@ -63,31 +64,72 @@
       <div v-show="!isShowTable">
         <el-form :inline="true" ref="form" label-width="80px" :model="attrInfo">
           <el-form-item label="属性名">
-            <el-input placeholder="请输入属性名" v-model="attrInfo.attrName"></el-input>
+            <el-input
+              placeholder="请输入属性名"
+              v-model="attrInfo.attrName"
+            ></el-input>
           </el-form-item>
         </el-form>
         <!-- 添加属性名按钮和取消 -->
-        <el-button type="primary" icon="el-icon-plus" @click="addAttrValue" :disabled="!attrInfo.attrName">添加属性值</el-button>
-        <el-button type="" @click="isShowTable=true">取消</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          @click="addAttrValue"
+          :disabled="!attrInfo.attrName"
+          >添加属性值</el-button
+        >
+        <el-button type="" @click="isShowTable = true">取消</el-button>
         <!-- 表格 -->
-        <el-table style="width: 100%;margin:20px 0px" border :data="attrInfo.attrValueList">
+        <el-table
+          style="width: 100%; margin: 20px 0px"
+          border
+          :data="attrInfo.attrValueList"
+        >
           <el-table-column label="序号" type="index" width="80" align="center">
           </el-table-column>
-          <el-table-column label="属性值名称" prop="prop" width="width" align="center">
-          <template slot-scope="{row}">
-              <el-input v-model="row.valueName" placeholder="请输入属性值名称" size="mini"></el-input>
-            
-          </template>
+          <el-table-column
+            label="属性值名称"
+            prop="prop"
+            width="width"
+            align="center"
+          >
+            <template slot-scope="{ row, $index}">
+              <!-- <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                size="mini"
+              ></el-input> -->
+              <!-- 这里结构需要用到span与input进行来回的切换 -->
+              <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                size="mini"
+                v-if="row.flag"
+                @blur="toLook(row)"
+                @keyup.native.enter="toLook(row)"
+                :ref="$index"
+              ></el-input>
+              <span
+                v-else
+                @click="toEdit(row, $index)"
+                style="display: block"
+                >{{ row.valueName }}</span
+              >
+            </template>
           </el-table-column>
           <el-table-column label="操作" prop="prop" width="width" align="left">
             <template slot-scope="{}">
-              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
             </template>
           </el-table-column>
         </el-table>
         <!-- 保存和取消按钮 -->
         <el-button type="primary">保存</el-button>
-        <el-button type="" @click="isShowTable=true">取消</el-button>
+        <el-button type="" @click="isShowTable = true">取消</el-button>
       </div>
     </el-card>
     <!-- <el-card></el-card> -->
@@ -95,6 +137,9 @@
 </template>
 
 <script>
+// 引入深拷贝
+import cloneDeep from "lodash/cloneDeep";
+
 export default {
   name: "Attr",
   data() {
@@ -105,13 +150,12 @@ export default {
       // 平台属性列表
       attrList: [],
       // 是否展示table
-      isShowTable: false,
+      isShowTable: true,
       //收集新增属性|修改属性使用的
       attrInfo: {
         attrName: "", //属性名
         attrValueList: [
           //属性值，因为属性值可以有多个因此用数组，每一个属性值都是一个对象需要attrId，valueName
-          
         ],
         categoryId: 0, //三级分类的id
         categoryLevel: 3, //因为服务器也需要区分几级id
@@ -152,7 +196,7 @@ export default {
       }
     },
     //添加属性按钮回调
-    addAttr(){
+    addAttr() {
       //切换table显示与隐藏
       this.isShowTable = false;
       //清除数据
@@ -166,13 +210,67 @@ export default {
         categoryLevel: 3, //因为服务器也需要区分几级id
       };
     },
-    addAttrValue(){
+    addAttrValue() {
       // 向list中push数据
       this.attrInfo.attrValueList.push({
-        attrId: undefined,         
-        valueName:  ""
-      })
-    }
+        attrId: this.attrInfo.id, //对于修改某一个属性的时候，可以在已有的属性值基础之上新增新的属性值（新增属性值的时候，需要把已有的属性的id带上）
+        valueName: "",
+        flag: true,
+        //flag属性：给每一个属性值添加一个标记flag，用户切换查看模式与编辑模式，好处，每一个属性值可以控制自己的模式切换
+        //当前flag属性，响应式数据（数据变化视图跟着变化）
+      });
+    },
+    updateAttr(row) {
+      // console.log(row);
+      //isShowTable变为false
+      this.isShowTable = false;
+      //将选中的属性赋值给attrInfo
+      //由于数据结构当中存在对象里面套数组，数组里面有套对象，因此需要使用深拷贝解决这类问题
+      //深拷贝，浅拷贝在面试的时候出现频率很高，切记达到手写深拷贝与浅拷贝
+      this.attrInfo = cloneDeep(row);
+      //在修改某一个属性的时候，将相应的属性值元素添加上flag这个标记
+      this.attrInfo.attrValueList.forEach((item) => {
+      //这样书写也可以给属性值添加flag自动，但是会发现视图不会跟着变化（因为flag不是响应式数据）
+      //因为Vue无法探测普通的新增 property,这样书写的属性并非响应式属性（数据变化视图跟这边）
+      //第一个参数:对象  第二个参数:添加新的响应式属性  第三参数：新的属性的属性值
+      this.$set(item, "flag", false);
+      });
+    },
+
+    //失却焦点的事件---切换为查看模式，展示span
+    toLook(row) {
+      // 如果属性值为空不能作为新的属性值，需要给用户提示，让他输入一个其他的属性值
+      if (row.valueName.trim() == "") {
+        this.$message("请你输入一个正常的属性值");
+        return;
+      }
+      //新增的属性值不能与已有的属性值重复
+      let isRepat = this.attrInfo.attrValueList.some((item) => {
+        //需要将row从数组里面判断的时候去除
+        //row最新新增的属性值【数组的最后一项元素】
+        //判断的时候，需要把已有的数组当中新增的这个属性值去除
+        if (row !== item) {
+          return row.valueName == item.valueName;
+        }
+      });
+
+      if (isRepat) return;
+      // row：形参是当前用户添加的最新的属性值
+      // 当前编辑模式变为查看模式【让input消失，显示span】
+      row.flag = false;
+    },
+    //点击span的回调，变为编辑模式
+    toEdit(row, index) {
+      row.flag = true;
+      //获取input节点，实现自动聚焦
+      //需要注意：点击span的时候，切换为input变为编辑模式，但是需要注意，对于浏览器而言，页面重绘与重拍耗时间的
+      //点击span的时候，重绘重拍一个input它是需要耗费事件，因此我们不可能一点击span立马获取到input
+      //$nextTick,当节点渲染完毕了，会执行一次
+      this.$nextTick(() => {
+        //获取相应的input表单元素实现聚焦
+        this.$refs[index].focus();
+      });
+    },
   },
 };
 </script>
