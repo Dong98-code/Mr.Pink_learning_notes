848c300d6e64ed97dc3adcf034163be248ba3378
”Dong98-code“ <1094108253@qq.com>
2022-07-16 16:16:53 +0800

    平台属性 与修改属性静态组件

--- 尚品汇项目/shp_houtai/vue-admin-template/src/views/product/Attr/index.vue
+++ 尚品汇项目/shp_houtai/vue-admin-template/src/views/product/Attr/index.vue
@@ -4,51 +4,84 @@
       <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
     </el-card>
     <el-card>
-      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
       <!-- 表格部分 -->
-      <el-table style="width: 100%" border :data="attrList">
-        <el-table-column align="center" type="index" width="80" label="序号">
-        </el-table-column>
-        <el-table-column
-          header-align="center"
-          align="center"
-          prop="attrName"
-          width="150"
-          label="属性列表"
+      <div v-show="isShowTable">
+        <!-- 添加禁用属性 -->
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          style="margin-bottom: 10px"
+          :disabled="!category3Id"
+          @click="isShowTable = false"
+          >添加属性</el-button
         >
-        </el-table-column>
-        <el-table-column header-align="center" prop="prop" label="属性值列表">
-          <template slot-scope="{ row }">
-            <el-tag
-              type="success"
-              v-for="attrValue in row.attrValueList"
-              :key="attrValue.id"
-              style="margin-left: 20px"
-              >{{ attrValue.valueName }}</el-tag
-            >
-          </template>
-        </el-table-column>
-        <el-table-column
-          header-align="center"
-          align="center"
-          prop="prop"
-          width="150"
-          label="操作"
-        >
-          <template slot-scope="{}">
-            <el-button
-              type="warning"
-              icon="el-icon-edit"
-              size="mini"
-            ></el-button>
-            <el-button
-              type="danger"
-              icon="el-icon-delete"
-              size="mini"
-            ></el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+        <el-table style="width: 100%" border :data="attrList">
+          <el-table-column align="center" type="index" width="80" label="序号">
+          </el-table-column>
+          <el-table-column
+            header-align="center"
+            align="center"
+            prop="attrName"
+            width="150"
+            label="属性列表"
+          >
+          </el-table-column>
+          <el-table-column header-align="center" prop="prop" label="属性值列表">
+            <template slot-scope="{ row }">
+              <el-tag
+                type="success"
+                v-for="attrValue in row.attrValueList"
+                :key="attrValue.id"
+                style="margin-left: 20px"
+                >{{ attrValue.valueName }}</el-tag
+              >
+            </template>
+          </el-table-column>
+          <el-table-column
+            header-align="center"
+            align="center"
+            prop="prop"
+            width="150"
+            label="操作"
+          >
+            <template slot-scope="{}">
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+              ></el-button>
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 添加属性和修改属性 -->
+      <div v-show="!isShowTable">
+        <el-form :inline="true" ref="form" label-width="80px">
+          <el-form-item label="属性名">
+            <el-input placeholder="请输入属性名"></el-input>
+          </el-form-item>
+        </el-form>
+        <!-- 添加属性名按钮和取消 -->
+        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button type="" @click="isShowTable=true">取消</el-button>
+        <!-- 表格 -->
+        <el-table style="width: 100%;margin:20px 0px" border>
+          <el-table-column label="序号" type="index" width="80" align="center">
+          </el-table-column>
+          <el-table-column label="属性值名称" prop="prop" width="width" align="center">
+          </el-table-column>
+          <el-table-column label="操作" prop="prop" width="width" align="center">
+          </el-table-column>
+        </el-table>
+        <!-- 保存和取消按钮 -->
+        <el-button type="primary">保存</el-button>
+        <el-button type="" @click="isShowTable=true">取消</el-button>
+      </div>
     </el-card>
     <!-- <el-card></el-card> -->
   </div>
@@ -64,6 +97,8 @@ export default {
       category3Id: "",
       // 平台属性列表
       attrList: [],
+      // 是否展示table
+      isShowTable: false,
     };
   },
   methods: {
