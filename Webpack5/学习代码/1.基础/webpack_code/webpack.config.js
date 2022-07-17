2fb9f8e4881337090dbfae0ecbb32add40fb5e4b
”Dong98-code“ <1094108253@qq.com>
2022-07-17 16:19:13 +0800

    修改输出资源的名称和路径

--- Webpack5/学习代码/1.基础/webpack_code/webpack.config.js
+++ Webpack5/学习代码/1.基础/webpack_code/webpack.config.js
@@ -10,9 +10,10 @@ module.exports = {
     // path: 文件输出目录，必须是绝对路径
     // path.resolve()方法返回一个绝对路径
     // __dirname 当前文件的文件夹绝对路径
+    // 所有文件的输出路径
     path: path.resolve(__dirname, "dist"),
     // filename: 输出文件名
-    filename: "main.js",
+    filename: "static/js/main.js",
   },
   // 加载器
   module: {
@@ -43,6 +44,14 @@ module.exports = {
           dataUrlCondition: {
             maxSize: 10 * 1024 // 小于10kb的图片会被base64处理
           }
+        },
+        generator: {
+          // 将图片文件输出到 static/imgs 目录中
+          // 将图片文件命名 [hash:8][ext][query]
+          // [hash:8]: hash值取8位
+          // [ext]: 使用之前的文件扩展名
+          // [query]: 添加之前的query参数
+          filename:"static/imgs/[hash:10][ext][query]"
         }
       },
     ],
