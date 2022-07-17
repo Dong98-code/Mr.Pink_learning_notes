3c9005cb155c65c66744ad1be6e27adac739f11f
”Dong98-code“ <1094108253@qq.com>
2022-07-17 17:12:55 +0800

    html配置

--- Webpack5/学习代码/1.基础/webpack_code/webpack.config.js
+++ Webpack5/学习代码/1.基础/webpack_code/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 // 插件需要引用
 const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
@@ -88,6 +89,11 @@ module.exports = {
     new ESLintWebpackPlugin({
       // 指定检查文件的根目录
       context: path.resolve(__dirname, "src"),
+    }),
+    new HtmlWebpackPlugin({
+      // 以 public/index.html 为模板创建文件
+      // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+      template: path.resolve(__dirname, "public/index.html"),
     }),
   ],
   // 模式
