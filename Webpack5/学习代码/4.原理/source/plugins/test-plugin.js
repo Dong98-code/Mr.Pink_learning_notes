089e05fed3afff5c462f88c81d1c67aa9d810b1b
”Dong98-code“ <1094108253@qq.com>
2022-07-19 20:07:57 +0800

    emit make 等几个hooks

--- Webpack5/学习代码/4.原理/source/plugins/test-plugin.js
+++ Webpack5/学习代码/4.原理/source/plugins/test-plugin.js
@@ -7,6 +7,56 @@ class TestPlugin {
     // 3. 遍历所有插件，调用插件的 apply 方法
     apply(compiler) {
         console.log("test-plugin compiler");
+
+        // environment 同步钩子函数
+        compiler.hooks.environment.tap('TestPlugin', () => {
+            console.log("test-plugin environment");
+        });
+
+        // 从文档可知, emit 是 AsyncSeriesHook, 也就是异步串行钩子，特点就是异步任务顺序执行
+        // 异步串行 输出 111 222 333
+        compiler.hooks.emit.tapAsync("TestPlugin", (compilation, callback) => {
+            setTimeout(() => {
+                console.log("compiler.emit() 111");
+                callback();
+            }, 3000);
+        });
+
+        compiler.hooks.emit.tapAsync("TestPlugin", (compilation, callback) => {
+            setTimeout(() => {
+                console.log("compiler.emit() 222");
+                callback();
+            }, 2000);
+        });
+
+        compiler.hooks.emit.tapAsync("TestPlugin", (compilation, callback) => {
+            setTimeout(() => {
+                console.log("compiler.emit() 333");
+                callback();
+            }, 1000);
+        });
+        // 从文档可知, make 是 AsyncParalelHook, 也就是异步并行钩子，特点就是异步任务执行
+        // 异步串行 输出 111 222 333
+        compiler.hooks.make.tapAsync("TestPlugin", (compilation, callback) => {
+            setTimeout(() => {
+                console.log("compiler.make() 111");
+                callback();
+            }, 3000);
+        });
+
+        compiler.hooks.make.tapAsync("TestPlugin", (compilation, callback) => {
+            setTimeout(() => {
+                console.log("compiler.make() 222");
+                callback();
+            }, 2000);
+        });
+
+        compiler.hooks.make.tapAsync("TestPlugin", (compilation, callback) => {
+            setTimeout(() => {
+                console.log("compiler.make() 333");
+                callback();
+            }, 1000);
+        });
     }
 }
 
