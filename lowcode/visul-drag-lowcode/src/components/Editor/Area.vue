e5c98e0e38e221375b43554803bb9514745402c1
”Dong98-code“ <1094108253@qq.com>
2022-08-10 00:42:40 +0800

    feat:area组件

---
+++ lowcode/visul-drag-lowcode/src/components/Editor/Area.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="area"
+  :style="{
+        left:start.x +'px',
+        top:start.y + 'px',
+        width: width + 'px',
+        height:Height + 'px'
+    }">
+    
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        start:{
+            type:Object,
+            default: () => {} // 传入的是组件的起始位置信息
+        },
+        width: {
+            type:Number,
+            default:0
+        },
+        Height: {
+            type:Number,
+            default:0
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.area {
+    border: 1px solid skyblue;
+    position: absolute;
+}
+</style>
