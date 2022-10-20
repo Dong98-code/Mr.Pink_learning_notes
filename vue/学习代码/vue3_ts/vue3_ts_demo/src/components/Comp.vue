084e49ccbfd1936592792757bbbc81501ceac57b
”Dong98-code“ <1094108253@qq.com>
2022-10-20 19:35:19 +0800

    feat:断言学习

--- vue/学习代码/vue3_ts/vue3_ts_demo/src/components/Comp.vue
+++ vue/学习代码/vue3_ts/vue3_ts_demo/src/components/Comp.vue
@@ -3,13 +3,37 @@
     xingddd
     {{counter}}
   </div>
+  <!-- 代办事项的内容 -->
+  <div v-for="(item, index) in items" :key="item.id">{{item.name}}</div>
 </template>
 
-<script lang='ts' setup>
+<!-- <script lang='ts' setup>
     import {ref} from "vue"
     const counter = ref(0)
-</script>
+</script> -->
 
+<script lang="ts">
+import { defineComponent } from "vue"
+
+type Todo = {
+    id: number,
+    name:string
+}
+export default defineComponent({
+    data() {
+        return {
+            counter: 1,
+            items: [] as Todo[] // 断言
+        }
+    },
+    created() {
+        this.items.push({
+            id: 1,
+            name:"xddd"
+        });
+    }
+})
+</script>
 <style scoped lang='less'>
     
 </style>
